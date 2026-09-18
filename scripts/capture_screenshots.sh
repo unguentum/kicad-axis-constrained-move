a5196bc2effeaacfd98aeda5387520f7dde37e14
unguentum <witherbones1@gmail.com>
2026-09-18 15:33:06 +0800

    Anchor constrained move at reference centre

--- scripts/capture_screenshots.sh
+++ scripts/capture_screenshots.sh
@@ -108,13 +108,14 @@ sleep 2
 scrot screenshots/move-aligned-dialog.png
 
 # Same X is the default: align centres on X and leave vertical movement free.
+xdotool mousemove --sync 958 543
 xdotool windowfocus "$plugin_window"
 xdotool key Return
 sleep 3
 xdotool windowfocus "$pcb_window"
-xdotool mousemove --sync 905 650
+xdotool mousemove --sync 958 650
 sleep 1
-xdotool mousemove --sync 905 650
+xdotool mousemove --sync 958 650
 sleep 2
 scrot screenshots/axis-constrained-move.png
 xdotool key Escape
